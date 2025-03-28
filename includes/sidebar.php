b70b08b12eb070e104e3e3862b1097f374492c28
Soloman Bhai <rushour10000@gmail.com>
2025-03-29 00:59:06 +0530

    Added sidebar.php file in includes folder

---
+++ includes/sidebar.php
@@ -0,0 +1,74 @@
+<aside class="sidebar">
+                    <div class="nav-section nav-left">
+                        <button class="nav-button menu-button">
+                            <i class="fa-solid fa-bars"></i>
+                        </button>
+                        <a href="#" class="nav-logo">
+                            <i class="fa-solid fa-play"></i>
+                            <h2 class="logo-text">VeePlay</h2>
+                        </a>
+                    </div>
+
+                <div class="link-container">
+                    <div class="link-section">
+                        <a href="#" class="link-item">
+                            <i class="fa-solid fa-house"></i> Home
+                        </a>
+                        <a href="#" class="link-item">
+                            <i class="fa-solid fa-video"></i> Shorts
+                        </a>
+                        <a href="#" class="link-item">
+                            <i class="fa-solid fa-briefcase"></i> Subscription
+                        </a>
+                    </div>
+                    <div class="section-separator"></div>
+
+                    <div class="link-section">
+                        <h4 class="section-title">You</h4>
+                        <a href="#" class="link-item">
+                            <i class="fa-brands fa-youtube"></i> Your Channel
+                        </a>
+                        <a href="#" class="link-item">
+                            <i class="fa-solid fa-clock-rotate-left"></i> History
+                        </a>
+                        <a href="#" class="link-item">
+                            <i class="fa-solid fa-clock"></i> Watch later
+                        </a>
+                    </div>
+                    <div class="section-separator"></div>
+
+                    <div class="link-section">
+                        <h4 class="section-title">Explore</h4>
+                        <a href="#" class="link-item">
+                            <i class="fa-solid fa-fire"></i> Trending
+                        </a>
+                        <a href="#" class="link-item">
+                            <i class="fa-solid fa-music"></i> Music
+                        </a>
+                        <a href="#" class="link-item">
+                            <i class="fa-solid fa-dice"></i> Gaming
+                        </a>
+                        <a href="#" class="link-item">
+                            <i class="fa-solid fa-football"></i> Sports
+                        </a>
+
+                    </div>
+                    <div class="section-separator"></div>
+
+                    <div class="link-section">
+                        <a href="#" class="link-item">
+                            <i class="fa-solid fa-gear"></i> Setting
+                        </a>
+                        <a href="#" class="link-item">
+                            <i class="fa-regular fa-file-lines"></i> Report
+                        </a>
+                        <a href="#" class="link-item">
+                            <i class="fa-solid fa-circle-question"></i> Help
+                        </a>
+                        <a href="#" class="link-item">
+                            <i class="fa-solid fa-triangle-exclamation"></i> Feedback
+                        </a>
+                    </div>
+                    <div class="section-separator"></div>
+                </div>
+             </aside>
