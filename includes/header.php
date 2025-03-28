8057fcc7863acfe6ec96562be461c91825b02098
Soloman Bhai <rushour10000@gmail.com>
2025-03-29 00:44:29 +0530

    Added header for the home page in includes folder

---
+++ includes/header.php
@@ -0,0 +1,36 @@
+<header>
+    <nav class="navbar">
+        <div class="nav-section nav-left">
+            <button class="nav-button menu-button">
+                <i class="fa-solid fa-bars"></i>
+            </button>
+            <a href="#" class="nav-logo">
+                <i class="fa-solid fa-play"></i>
+                <h2 class="logo-text">VeePlay</h2>
+            </a>
+        </div>
+
+        <div class="nav-section nav-center">
+            <form action="#" class="search-form">
+                <input type="search" placeholder="Search" required class="search-input">
+                <button class="nav-button search-button">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </button>
+            </form>
+            <button class="nav-button mic-button">
+                <i class="fa-solid fa-microphone"></i>
+            </button>
+        </div>
+
+        <div class="nav-section nav-right">
+            <button class="nav-button search-button">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </button>
+
+            <button class="nav-button theme-button">
+                <i class="fa-solid fa-moon"></i>
+            </button>
+            <img src="https://i.ytimg.com/vi/OORUHkgg4IM/maxresdefault.jpg" alt="User Image" class="user-image">
+        </div>
+    </nav>
+</header>
